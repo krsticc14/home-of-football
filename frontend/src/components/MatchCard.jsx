45758ee20913067f958cb50c7da7d08371977bce
krsticc14 <aleksandar.krstic021@gmail.com>
2026-05-12 14:22:14 +0200

    video fix 2

--- frontend/src/components/MatchCard.jsx
+++ frontend/src/components/MatchCard.jsx
@@ -1,18 +1,23 @@
 import "./MatchCard.css";
 
 function MatchCard({ match }) {
-
-  const safeVideo = match.video?.replace("http://", "https://");
-
   return (
     <div className="match-card">
       <h3 className="match-title">{match.title}</h3>
       <p className="match-competition">{match.competition?.name}</p>
 
-      <div
-        className="match-video"
-        dangerouslySetInnerHTML={{ __html: safeVideo }}
+      <img 
+      src={match.thumbnail}
+      alt={match.title}
+      className="match-thumbnail"
       />
+
+      <a 
+      href={match.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="watch-button"
+      > Watch Highlights</a>
     </div>
   );
 }
